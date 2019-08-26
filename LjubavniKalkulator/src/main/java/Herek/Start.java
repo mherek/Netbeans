b7a9c151117ff6da381def7f744a786bf5912622
Marko <markosolarcro@gmail.com>
2019-08-26 13:35:13 +0200

    Ljubavni kalkulator
    Alogirtam za dodavanje brojeva ne radi

---
+++ LjubavniKalkulator/src/main/java/Herek/Start.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Herek;
+
+import Herek.view.Izbornik;
+
+/**
+ *
+ * @author Isus
+ */
+public class Start {
+    public static void main(String[] args) {
+        new Izbornik().setVisible(true);
+    }
+ 
+}
